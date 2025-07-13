df9080c18cfdb673e17dc876a2c8267796cd85ce
LorenzoDantoni <lorenzo.dantoni@studenti.unipd.it>
2025-07-13 19:27:37 +0200

    Improved ui of contact page.

--- app/(frontend)/contatti/page.tsx
+++ app/(frontend)/contatti/page.tsx
@@ -19,7 +19,7 @@ const ContattiPage = () => {
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           {regionalContact && <RegionalContact {...regionalContact} />}
           <div className="mt-16">
-            <h2 className="text-4xl font-bold text-gray-900 text-center mb-12">
+            <h2 className="text-3xl font-bold text-gray-900 text-center mb-12">
               Federazioni Provinciali
             </h2>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
--- components/ContactCard.tsx
+++ components/ContactCard.tsx
@@ -15,20 +15,20 @@ const ContactCard: React.FC<ContactCardProps> = ({ federation, address, email, f
   return (
     <div className="bg-white rounded-lg shadow-lg overflow-hidden transform transition-transform duration-300 hover:scale-105">
       <div className="p-6">
-        <h2 className="text-3xl font-bold text-gray-800 mb-2">{federation}</h2>
-        <p className="text-gray-600 text-lg mb-4">{address}</p>
-        <a href={`mailto:${email}`} className="text-lg text-orange-500 hover:text-orange-600 transition-colors duration-300">{email}</a>
+        <h2 className="text-2xl font-bold text-gray-800 mb-2">{federation}</h2>
+        <p className="text-gray-600 text-md mb-4">{address}</p>
+        <a href={`mailto:${email}`} className="text-md text-orange-500 hover:text-orange-600 transition-colors duration-300">{email}</a>
         <div className="mt-4 flex flex-col space-y-2">
           {facebook && facebookTag && (
             <a href={facebook} target="_blank" rel="noopener noreferrer" className="flex items-center text-gray-600 hover:text-blue-600 transition-colors duration-300">
               <FaFacebook size={20} />
-              <span className="ml-2">{facebookTag}</span>
+              <span className="ml-2 text-md">{facebookTag}</span>
             </a>
           )}
           {instagram && instagramTag && (
             <a href={instagram} target="_blank" rel="noopener noreferrer" className="flex items-center text-gray-600 hover:text-pink-500 transition-colors duration-300">
               <FaInstagram size={20} />
-              <span className="ml-2">{instagramTag}</span>
+              <span className="ml-2 text-md">{instagramTag}</span>
             </a>
           )}
         </div>
